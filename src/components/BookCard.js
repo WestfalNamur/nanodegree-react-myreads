296568466de675eb334d2802b60da9325913d65b
Stefan Crummenerl <CrummenerlS@gmail.com>
2019-03-01 19:15:37 +0100

    shelfs

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -4,22 +4,24 @@ import '../utils/App.css'
 class BookCard extends Component {
   render() {
     return (
-      <div>
-        <ul>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
           {this.props.books.map((book) => (
             <li key={book.id}>
               <div className='book'>
-                <div 
-                  className="book-cover" 
-                  style={{ width: 128, height: 193, backgroundImage:
-                    `url(${book.imageLinks.smallThumbnail})`}}>
+                <div className="book-top">
+                  <div 
+                    className="book-cover" 
+                    style={{ width: 128, height: 193, backgroundImage:
+                      `url(${book.imageLinks.smallThumbnail})`}}>
+                  </div>
                 </div>
                 <div className="book-title">{book.title}</div>
                 <div className="book-authors">{book.authors}</div>
               </div>
             </li>
           ))}
-        </ul>
+        </ol>
       </div>
     )
   }
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,8 +4,25 @@ import BookCard from './BookCard'
 class Dashboard extends Component {
   render() {
     return (
-      <div>
-        <BookCard books={this.props.books}/>
+      <div className="list-books-content">
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">Currently reading</h2>
+          <BookCard books={this.props.books.filter(book =>
+            book.shelf === 'currentlyReading'
+          )}/>
+        </div>
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">Want to read</h2>
+          <BookCard books={this.props.books.filter(book =>
+            book.shelf === 'wantToRead'
+          )}/>
+        </div>
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">Read</h2>
+          <BookCard books={this.props.books.filter(book =>
+            book.shelf === 'read'
+          )}/>
+        </div>
       </div>
     )
   }
